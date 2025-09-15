fa6cb7227018ce9f6f7cf2daf95131516341f481
ShravanDev050 <coolshravan050@gmail.com>
2025-09-15 21:47:46 +0530

    added file permission in tf

--- local.tf
+++ local.tf
@@ -1,4 +1,5 @@
 resource "local_file" "firstprovision" {
     filename = "/Users/shravan/My_data/terraform_provision/firstprovision.txt"
     content = "This is my first provisioning configuration"
+    file_permission=700
 }
