9aa2d9566a1625bd36b662f07b9412b1296eacbc
ShravanDev050 <coolshravan050@gmail.com>
2025-09-15 21:41:47 +0530

    added first tf

---
+++ local.tf
@@ -0,0 +1,4 @@
+resource "local_file" "firstprovision" {
+    filename = "/Users/shravan/My_data/terraform_provision/firstprovision.txt"
+    content = "This is my first provisioning configuration"
+}
